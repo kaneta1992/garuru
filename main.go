7e32c6d57bfc83b29602a9aabb5bf41d6617ac6e
kaneta <kaneta1011@gmail.com>
2017-10-17 17:05:18 +0900

    formを取れるようにした

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/kaneta1992/simple-web-benchmarker/util/benchmarker"
+	"github.com/kaneta1992/simple-web-benchmarker/util/session"
+	"github.com/tonnerre/golang-pretty"
 )
 
 func main() {
-	num := 32
+	// num := 32
 
-	b := benchmarker.NewBenchmarker("http://xn--u9j013yjqe.xn--u8jxb0b.com/", num)
-	ret := b.Start(3)
+	// b := benchmarker.NewBenchmarker("http://xn--u9j013yjqe.xn--u8jxb0b.com/", num)
+	// ret := b.Start(3)
 
-	fmt.Printf("%v\n", ret)
+	// fmt.Printf("%v\n", ret)
+	s := session.NewSession()
+	req, _ := s.NewRequest("GET", "http://sed.jp/uploader.php", nil)
+	res, _ := s.SendRequest(req)
+	h, _ := benchmarker.NewHttpAnalyzer(res)
+	forms, _ := h.GetForms()
+	pretty.Printf("%v\n", *forms[0])
 }
--- util/benchmarker/htmlAnalyzer.go
+++ util/benchmarker/htmlAnalyzer.go
@@ -5,7 +5,7 @@ import (
 	_ "io/ioutil"
 	"net/http"
 	"net/url"
-	_ "strings"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -28,7 +28,7 @@ func NewHttpAnalyzer(response *http.Response) (*HttpAnalyzer, error) {
 }
 
 func (h *HttpAnalyzer) GetLinks() ([]*url.URL, error) {
-	urls := make([]*url.URL, 0)
+	urls := []*url.URL{}
 	h.document.Find("a").Each(func(_ int, s *goquery.Selection) {
 		href, exists := s.Attr("href")
 		if exists {
@@ -44,4 +44,63 @@ func (h *HttpAnalyzer) GetLinks() ([]*url.URL, error) {
 		return nil, errors.New("not exist link")
 	}
 	return urls, nil
+}
+
+type HtmlForm struct {
+	Action  string
+	Method  string
+	EncType string
+	Params  map[string][]string
+}
+
+func NewHtmlForm(action, method, enctype string) *HtmlForm {
+	h := &HtmlForm{
+		Action:  action,
+		Method:  method,
+		EncType: enctype,
+		Params:  make(map[string][]string),
+	}
+	return h
+}
+
+func (h *HttpAnalyzer) GetForms() ([]*HtmlForm, error) {
+	forms := []*HtmlForm{}
+	h.document.Find("form").Each(func(_ int, fs *goquery.Selection) {
+		action, exists := fs.Attr("action")
+		if !exists {
+			return
+		}
+		method, exists := fs.Attr("method")
+		// POST以外のformは考慮していない
+		if !exists || !strings.EqualFold(method, "post") {
+			return
+		}
+		enctype, exists := fs.Attr("enctype")
+		if !exists {
+			enctype = "application/x-www-form-urlencoded"
+		}
+		form := NewHtmlForm(action, method, enctype)
+		fs.Find("input,select,textarea").Each(func(_ int, s *goquery.Selection) {
+			name, exists := s.Attr("name")
+			if exists {
+				if _, exists = form.Params[name]; !exists {
+					form.Params[name] = []string{}
+				}
+				t, _ := s.Attr("type")
+				switch t {
+				case "hidden":
+					if v, exists := s.Attr("value"); exists {
+						form.Params[name] = append(form.Params[name], v)
+					}
+				}
+			}
+		})
+		forms = append(forms, form)
+	})
+
+	size := len(forms)
+	if size < 1 {
+		return nil, errors.New("not exist form")
+	}
+	return forms, nil
 }
